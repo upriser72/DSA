208f49bcae9cc782394fa878684de48f2cd58acc
vanshika8692316 <vs8692316@gmail.com>
2023-06-20 17:48:15 +0530

    new

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void printname( string name)
+{
+   cout<<"hi "<<name;
+}
+int main()
+{
+   string name;
+   cin>>name;
+   printname(name);
+   return 0;
+}
